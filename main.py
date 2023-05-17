c891f247ecc1476f65230e7b3634e79d7fa82aa1
Johannes C. Mayer <j.c.mayer240@gmail.com>
2023-05-17 07:38:21 +0200

    global locking mechanism

--- main.py
+++ main.py
@@ -7,24 +7,49 @@ Note: ssml must be well-formed according to:
 from pathlib import Path
 import os
 import argparse
+import datetime
+import time
 import click
 from google.cloud import texttospeech
 
+def get_timestamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f")
 
-#parser = argparse.ArgumentParser(description='Process some integers.')
-#parser.add_argument('text', const=sum, default="No text was given.",
-#                    help='sum the integers (default: find the max)')
+id = get_timestamp()
+
+# Paths
+project_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+audio_dir = project_dir / 'audio_file_cache'
+locks_dir = project_dir / 'locks'
+lock_file = locks_dir / id
+
+locks_dir.mkdir(exist_ok=True)
+
+wait_locks = list(locks_dir.iterdir())
+lock_file.touch()
+
+# Arguments
+parser = argparse.ArgumentParser(description='Google Text to speech. Nightcored.')
+parser.add_argument('--speed', type=float, help="Speed of the generated voice")
+parser.add_argument('--pitch', type=float, help="Pitch of the generated voice")
+parser.add_argument('text',  type=str, nargs='+', help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+args.text = " ".join(args.text)
 
 
-project_dir = os.path.dirname(os.path.realpath(__file__))
-audio_cache_dir = f"{project_dir}/audio_file_cache"
-
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-# TODO use this
 female_wavenet_voices_us = [f"en-US-Wavenet-{voice}" for voice in ["C","E","F","G","H"]]
 female_standard_voices_us = [f"en-US-Standard-{voice}" for voice in ["F"]]
-tempo = 1
+
+pitch_coef = args.pitch if args.pitch else 1
+speak_rate = args.speed if args.speed else 1
+
+def wait_for_lock():
+    for lock in wait_locks:
+        if lock.name == lock_file.name:
+            continue
+        while lock.is_file():
+            time.sleep(0.05)
 
 class Speaker:
     def __init__(self):
@@ -35,38 +60,41 @@ class Speaker:
             raise NotImplemented()
         #ssml = '<prosody rate="slow" pitch="2st">Can you hear me now? Yes, that is right! I am here.</prosody>'
 
-        cache_dir = f"{audio_cache_dir}/{self.unique_name}"
-        audio_file = f"{cache_dir}/{text}.mp3"
-        audio_file_pp = f"{cache_dir}/{text}_pp.mp3"
+        file_name = id
 
-        if not os.path.isfile(audio_file_pp):
-            client = texttospeech.TextToSpeechClient()
-            synthesis_input = texttospeech.SynthesisInput(text=text)
-            
-            response = client.synthesize_speech(
-                input=synthesis_input, voice=self.voice, audio_config=self.audio_config
-            )
+        audio_file = f"{audio_dir}/{file_name}.mp3"
+        audio_file_pp = f"{audio_dir}/{file_name}_pp.mp3"
 
-            Path(cache_dir).mkdir(parents=True, exist_ok=True)
-
-            if not os.path.isdir(audio_cache_dir):
-                os.mkdir(audio_cache_dir, )
-            with open(audio_file, "wb") as out:
-                out.write(response.audio_content)
-
-            os.system(f'ffmpeg -loglevel quiet -i "{audio_file}" '
-                f'-filter:a "atempo={self.ff_tempo},asetrate=44100*{self.ff_rate_coef}" '
-                f'"{audio_file_pp}" -y')
-            os.remove(audio_file)
+        client = texttospeech.TextToSpeechClient()
+        synthesis_input = texttospeech.SynthesisInput(text=text)
         
+        response = client.synthesize_speech(
+            input=synthesis_input, voice=self.voice, audio_config=self.audio_config
+        )
+
+        Path(audio_dir).mkdir(parents=True, exist_ok=True)
+
+        if not os.path.isdir(audio_dir):
+            os.mkdir(audio_dir, )
+        with open(audio_file, "wb") as out:
+            out.write(response.audio_content)
+
+        os.system(f'ffmpeg -loglevel quiet -i "{audio_file}" '
+            f'-filter:a "atempo={self.ff_tempo},asetrate=44100*{self.ff_rate_coef}" '
+            f'"{audio_file_pp}" -y')
+        os.remove(audio_file)
+        
+        wait_for_lock()
         os.system(f'mpv --no-resume-playback "{audio_file_pp}" > /dev/null')
+        lock_file.unlink()
+        os.remove(audio_file_pp)
 
 
 class Alice(Speaker):
     def __init__(self):
         self.unique_name = "Alice"
-        self.ff_rate_coef = 1.20
-        self.ff_tempo = 1.00
+        self.ff_rate_coef = 1.05
+        self.ff_tempo = 1.0
         self.voice = texttospeech.VoiceSelectionParams(
             name = "en-US-Wavenet-H",
             language_code="en-US",
@@ -74,8 +102,8 @@ class Alice(Speaker):
         )
         self.audio_config = texttospeech.AudioConfig(
             audio_encoding=texttospeech.AudioEncoding.MP3,
-            speaking_rate = 0.8,
-            pitch = 1,
+            speaking_rate = 1.5 * speak_rate,
+            pitch = 0.6,
             volume_gain_db = 0,
             sample_rate_hertz = 44100,
             #"effectsProfileId": [
@@ -94,7 +122,7 @@ class Test(Speaker):
         )
         self.audio_config = texttospeech.AudioConfig(
             audio_encoding=texttospeech.AudioEncoding.MP3,
-            speaking_rate = 0.8,
+            speaking_rate = 0.8 * speak_rate,
             pitch = 1,
             volume_gain_db = 0,
             sample_rate_hertz = 44100,
@@ -103,13 +131,9 @@ class Test(Speaker):
             #]
         )
 
-@click.command()
-@click.argument('text', type=click.STRING, nargs=-1)
-def main(text):
-    text = " ".join(text)
-
+def main():
     speaker = Alice()
-    speaker.speak(text)
+    speaker.speak(args.text)
 
 if __name__ == "__main__":
     main()
