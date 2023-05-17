b62d5bb902febc44fda983d410cd1d8ed92c295e
Johannes C. Mayer <j.c.mayer240@gmail.com>
2023-05-17 17:29:07 +0200

    mpv speedup

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ lock_file = locks_dir / id
 
 locks_dir.mkdir(exist_ok=True)
 
-wait_locks = list(locks_dir.iterdir())
-lock_file.touch()
 
 # Arguments
 parser = argparse.ArgumentParser(description='Google Text to speech. Nightcored.')
@@ -41,15 +39,12 @@ args.text = " ".join(args.text)
 female_wavenet_voices_us = [f"en-US-Wavenet-{voice}" for voice in ["C","E","F","G","H"]]
 female_standard_voices_us = [f"en-US-Standard-{voice}" for voice in ["F"]]
 
-pitch_coef = args.pitch if args.pitch else 1
-speak_rate = args.speed if args.speed else 1
-
 def wait_for_lock():
     for lock in wait_locks:
         if lock.name == lock_file.name:
             continue
         while lock.is_file():
-            time.sleep(0.05)
+            time.sleep(0.005)
 
 class Speaker:
     def __init__(self):
@@ -85,7 +80,18 @@ class Speaker:
         os.remove(audio_file)
         
         wait_for_lock()
-        os.system(f'mpv --no-resume-playback "{audio_file_pp}" > /dev/null')
+
+        # We calculate a speedup factor based on the number of sheduled files
+        # We want to catch up if there are many files sheduled for speaking.
+        MAX_SPEED = 1.3
+        MIN_SPEED = 1.0
+        UPPER_RANGE = 3
+
+        speed_coef = MIN_SPEED + (len(list(locks_dir.iterdir())) - 1) / UPPER_RANGE * (MAX_SPEED - MIN_SPEED)
+        speed_coef = min(MAX_SPEED, max(MIN_SPEED, speed_coef))
+        os.system(f'notify-send "speedup: {speed_coef}"')
+
+        os.system(f'mpv --speed {speed_coef} --no-resume-playback "{audio_file_pp}" > /dev/null')
         lock_file.unlink()
         os.remove(audio_file_pp)
 
@@ -102,7 +108,7 @@ class Alice(Speaker):
         )
         self.audio_config = texttospeech.AudioConfig(
             audio_encoding=texttospeech.AudioEncoding.MP3,
-            speaking_rate = 1.5 * speak_rate,
+            speaking_rate = 1.4 * speak_rate,
             pitch = 0.6,
             volume_gain_db = 0,
             sample_rate_hertz = 44100,
@@ -135,5 +141,14 @@ def main():
     speaker = Alice()
     speaker.speak(args.text)
 
+wait_locks = list(locks_dir.iterdir())
+lock_file.touch()
+
+pitch_coef = args.pitch if args.pitch else 1
+speak_rate = args.speed if args.speed else 1
+
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    finally:
+        lock_file.unlink()
