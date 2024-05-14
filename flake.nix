c7dda3b3e1a99535efb2e1841bdf4d982a57eb1d
Johannes C. Mayer <j.c.mayer240@gmail.com>
2024-05-15 00:07:20 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "gsay";
+  inputs = {};
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "hello";
+        src = self;
+        buildPhase = "gcc -o hello ./hello.c";
+        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+      };
+  };
+}
