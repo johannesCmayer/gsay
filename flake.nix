b3008780f16725bedae610eb6a1e2f5a0acf9cc6
Johannes C. Mayer <j.c.mayer240@gmail.com>
2024-05-15 00:59:14 +0200

    flake final

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "gsay";
-  inputs = {};
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default =
       # Notice the reference to nixpkgs here.
