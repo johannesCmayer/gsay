5404a59cf166ed3073e09b25ece43b427b64c943
Johannes C. Mayer <j.c.mayer240@gmail.com>
2024-05-15 00:36:48 +0200

    -

--- flake.nix
+++ flake.nix
@@ -5,6 +5,6 @@
     packages.x86_64-linux.default =
       # Notice the reference to nixpkgs here.
       with import nixpkgs { system = "x86_64-linux"; };
-      pkgs.callPackage ./app.nix {}
+      pkgs.callPackage ./app.nix {};
   };
 }
